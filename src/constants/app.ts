bec0accb517e5c8e8a11de5f07c484fa95403ea3
ibabkov <contact@ibabkov.com>
2026-05-20 15:27:21 -0400

    feat: customize landing page

--- src/constants/app.ts
+++ src/constants/app.ts
@@ -3,6 +3,9 @@ import { Language } from '../types/language';
 export const DISPLAY_APP_TITLE = 'DJI Mavic Pro AR Demo';
 export const APP_TITLE = 'DJI Mavic Pro AR Demo';
 export const APP_DESCRIPTION =
-	'An interactive online demo showcasing an animated 3D model of the DJI Mavic Pro drone hovering in augmented reality (AR). This AR experience is mobile-exclusive.';
+	'An interactive online demo showcasing an animated 3D model of the DJI Mavic Pro drone that follows you in augmented reality (AR). This AR experience is mobile-exclusive.';
 
 export const LANGUAGE: Language = 'en';
+
+export const LANDING_URL = 'https://www.ibabkov.com/projects/dji-mavic-pro-ar-demo/';
+export const LANDING_LOGO_SRC = '/landing-logo.jpeg';
